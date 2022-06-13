fca263911dc877e7d2b35252a740145f683bfb13
Tara-Lightner <tlightner23@gmail.com>
2022-06-13 11:01:16 -0400

    adding images

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -32,6 +32,8 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 
+SELECT * FROM employees;
+
 -- Create Department Managers Table
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
@@ -43,6 +45,7 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+SELECT * FROM dept_manager;
 -- Create Salaries Table
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
@@ -52,6 +55,8 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+SELECT * FROM salaries;
 
 -- Create Department Employee Table
 CREATE TABLE dept_emp (
@@ -75,6 +80,7 @@ CREATE TABLE titles (
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no, title, from_date));
 
+SELECT * FROM titles
 -- 7.2.3 Import Data
 --Query for Confirmation
 SELECT * FROM departments;
@@ -134,6 +140,8 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info
 
 -- Export Results Table
 
@@ -342,6 +350,107 @@ ON (de.dept_no = d.dept_no)
 WHERE dept_name IN ('Sales', 'Development');
 
 SELECT * FROM salesanddevteams_retirementinfo
+---------------------------------------
+-- Deliverable 1
+-- 1.) Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+-- 2.) Retrieve the title, from_date, and to_date columns from the Titles table.
+-- 3.) Create a new table using the INTO clause.
+-- 4.) Join both tables on the primary key.  ??? WHAT KIND OF JOIN SHOULD I BE DOING?
+-- 5.) Filter the data on the birth_date column to retrieve the employees who were born between 1952 and 1955. 
+-- Then, order by the employee number.
+
+SELECT  e.emp_no,
+        e.first_name,
+        e.last_name,
+        t.title,
+        t.from_date,
+        t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM retirement_titles;
+
+
+-- 9.) Retrieve the employee number, first and last name, and title columns from the Retirement Titles table.
+-- -- These columns will be in the new table that will hold the most recent title of each employee.
+-- 10.) Use the DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by....
+--  the ON () clause.
+-- 11.)Exclude those employees that have already left the company by filtering on to_date to keep only those dates that are....
+--equal to '9999-01-01'.
+-- 12.) Create a Unique Titles table using the INTO clause.
+-- 13.) Sort the Unique Titles table in ascending order by the employee number and descending order by the last date (i.e., to_date)....
+-- of the most recent title.
+
+SELECT DISTINCT ON (emp_no)
+	rt.emp_no,
+    rt.first_name,
+    rt.last_name,
+    rt.title
+INTO Unique_Titles
+FROM retirement_titles as rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM Unique_Titles
+
+-- 14.) Export the Unique Titles table as unique_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+-- 15.) Before you export your table, confirm that it looks like this image:
+-- 16.) Write another query in the Employee_Database_challenge.sql file to retrieve the number of employees by their....
+-- most recent job title who are about to retire.
+-- 17) First, retrieve the number of titles from the Unique Titles table.
+-- 18.) Then, create a Retiring Titles table to hold the required information.
+-- 19.) Group the table by title, then sort the count column in descending order.
+-- 20.) Export the Retiring Titles table as retiring_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+-- 21.) Before you export your table, confirm that it looks like this image:
+-- 22.) Save your Employee_Database_challenge.sql file in your Queries folder in the Pewlett-Hackard folder.
+
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+ --------
+ DELEVERABLE 2
+ -------
+ 
+-- Deliverable 2
+-- 1.) Retrieve the emp_no, first_name, last_name, and birth_date columns from the Employees table.
+-- 2.) Retrieve the from_date and to_date columns from the Department Employee table.
+-- 3.) Retrieve the title column from the Titles table.
+-- 4.) Use a DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by....
+-- the ON () clause.
+-- 5.) Create a new table using the INTO clause.
+-- 6.) Join the Employees and the Department Employee tables on the primary key.
+-- 7.) Join the Employees and the Titles tables on the primary key.
+-- 8.) Filter the data on the to_date column to all the current employees, then filter the data on the birth_date columns to....
+-- get all the employees whose birth dates are between January 1, 1965 and December 31, 1965.
+-- 9.) Order the table by the employee number.
+-- 10.) Export the Mentorship Eligibility table as mentorship_eligibilty.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+-- 11.) Before you export your table, confirm that it looks like this image:
+
+SELECT DISTINCT ON (emp_no)
+		e.emp_no,
+        e.first_name,
+        e.last_name,
+        e.birth_date,
+        de.from_date,
+        de.to_date,
+        t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+    INNER JOIN dept_emp AS de
+        ON (e.emp_no = de.emp_no)
+    INNER JOIN titles AS t
+        ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+
 
 -- *** All talbes listed for Query for Confirmation
 SELECT * FROM departments;
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 ## Background
 ## Deliverables
     Deliverable 1: The Number of Retiring Employees by Title
+
     Deliverable 2: The Employees Eligible for the Mentorship Program
     Deliverable 3: A written report on the employee database analysis (README.md)
 ## Files
